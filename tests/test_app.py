ee035a1d87adf07c31dea31ad74669be454a3014
Steven-Herrera <stevenherrera46375@utexas.edu>
2024-05-19 21:37:13 -0500

    integrate app with streamlit, add tests, update Dockerfile

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,36 @@
 """A module for testing the app.py module."""
 import os
 import pandas as pd
-from app import app
+import app
 
 def test_single_page():
+    """Test the app function with a video that has a single page of comments."""
     DEVELOPER_KEY = os.environ['DEVELOPER_KEY']
     youtube_video_id = "wJhYWbEi6NQ"
 
-    df = app(DEVELOPER_KEY, youtube_video_id)
+    df = app.app(DEVELOPER_KEY, youtube_video_id)
     assert isinstance(df, pd.DataFrame)
 
 def test_multi_page():
+    """Test the app function with a video that has multiple pages of comments."""
     DEVELOPER_KEY = os.environ['DEVELOPER_KEY']
     youtube_video_id = "bn_KRzohcAo"
 
-    df = app(DEVELOPER_KEY, youtube_video_id)
+    df = app.app(DEVELOPER_KEY, youtube_video_id)
     assert isinstance(df, pd.DataFrame)
+
+def test_bad_key():
+    """Test the app function with a bad key."""
+    DEVELOPER_KEY = "bad_key"
+    youtube_video_id = "wJhYWbEi6NQ"
+
+    df = app.app(DEVELOPER_KEY, youtube_video_id)
+    assert df == 'API key not valid. Please pass a valid API key.'
+
+def test_bad_video_id():
+    """Test the app function with a bad video id."""
+    DEVELOPER_KEY = os.environ['DEVELOPER_KEY']
+    youtube_video_id = "bad_video_id"
+
+    df = app.app(DEVELOPER_KEY, youtube_video_id)
+    assert df == "No video found. Are you sure the URL is correct?"
